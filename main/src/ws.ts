ea8422cf037066844ea2a3fad2356a6b2612cd14
Anurag12133 <anurajput62616@gmail.com>
2025-03-10 00:12:01 +0530

    add : handler function added in webscoket logic to handle specific repl

--- main/src/ws.ts
+++ main/src/ws.ts
@@ -2,7 +2,9 @@
 import { TerminalManager } from "./terminal";
 import {Server as HttpServer} from "http";
 import {Server, Socket} from "socket.io";
-import { fetchDirData, fetchFileContent, saveFile } from "./file";
+import { fetchDirData, fetchFileData, saveFileData } from "./file";
+import { saveToS3 } from "./s3";
+
 
 
 const terminalManager = new TerminalManager();
@@ -27,6 +29,41 @@ export function initWs(httpServer: HttpServer) {
         socket.emit("loaded", {
             rootContent: await fetchDirData("/workspace", "")
         });
-
+        initHandlers(socket, replId);
     })
+}
+function initHandlers(socket: Socket, replId : string) {
+    socket.on("disconnect", () => {
+        console.log("User Disconnected");
+    })
+    socket.on("fetchDir", async (dir: string, callback) => {
+        const dirPath = `/workspace/${dir}`;
+        const contents = await fetchDirData(dirPath, dir);
+        callback(contents);
+    });
+
+    socket.on("fetchContent", async ({ path: filePath }: { path: string }, callback) => {
+        const fullPath = `/workspace/${filePath}`;
+        const data = await fetchFileData(fullPath);
+        callback(data);
+    });
+    socket.on("updateContent", async ({ path: filePath, content }: { path: string, content: string }) => {
+        const fullPath =  `/workspace/${filePath}`;
+        await saveFileData(fullPath, content);
+        await saveToS3(`code/${replId}`, filePath, content);
+    });
+
+    socket.on("requestTerminal", async () => {
+        terminalManager.createPty(socket.id, replId, (data, id) => {
+            socket.emit('terminal', {
+                data: Buffer.from(data,"utf-8")
+            });
+        });
+    });
+
+    socket.on("terminalData", async ({ data }: { data: string, terminalId: number }) => {
+        terminalManager.write(socket.id, data);
+    });
+
+
 }
