f2890641e67be38f579e3347bf72f58ddc32cd57
Anurag12133 <anurajput62616@gmail.com>
2025-03-09 23:45:48 +0530

    add:code to intiate the websocket

--- main/src/ws.ts
+++ main/src/ws.ts
@@ -1,4 +1,32 @@
 
 import { TerminalManager } from "./terminal";
+import {Server as HttpServer} from "http";
+import {Server, Socket} from "socket.io";
+import { fetchDirData, fetchFileContent, saveFile } from "./file";
+
 
 const terminalManager = new TerminalManager();
+
+export function initWs(httpServer: HttpServer) {
+    const io = new Server(httpServer, {
+        cors : {
+            origin:"",
+            methods:["GET", "POST"]
+        }
+    })
+    io.on("connection", async (socket) => {
+        const host = socket.handshake.headers.host;
+        console.log(`host is: ${host}`);
+
+        const replId = host?.split('.')[0];
+        if(!replId) {
+            socket.disconnect();
+            terminalManager.clear(socket.id);
+            return;
+        }
+        socket.emit("loaded", {
+            rootContent: await fetchDirData("/workspace", "")
+        });
+
+    })
+}
