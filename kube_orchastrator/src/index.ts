64f5a8d94fcbbae5098904262f286c0f2d031990
Anurag12133 <anurajput62616@gmail.com>
2025-03-11 22:28:33 +0530

    add: Updated utility function to handle multi-document YAML files

--- kube_orchastrator/src/index.ts
+++ kube_orchastrator/src/index.ts
@@ -14,3 +14,15 @@ kubeconfig.loadFromDefault();
 const coreV1Api = kubeconfig.makeApiClient(CoreV1Api);
 const appsV1Api = kubeconfig.makeApiClient(AppsV1Api);
 const networkingV1Api = kubeconfig.makeApiClient(NetworkingV1Api);
+
+const readAndParseKubeYaml = (filePath: string, replId: string):Array<any> => {
+    const fileContent = fs.readFileSync(filePath, "utf8")
+    const docs = yaml.parseAllDocuments(fileContent).map((doc) => {
+        let docString = doc.toString();
+        const regex = new RegExp(`service_name`, 'g');
+        docString = docString.replace(regex, replId);
+        return yaml.parse(docString);
+    })
+    return docs;
+
+}
